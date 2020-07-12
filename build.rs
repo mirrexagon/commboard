d992e71535cb9a7759d768865a588bb13ee2f4db
Andrew Abbott <mirrexagon@mirrexagon.com>
2020-07-12 22:10:48 +1000

    Add build script to build UI

---
+++ build.rs
@@ -0,0 +1,29 @@
+use std::process::Command;
+use walkdir::WalkDir;
+
+const UI_DIR: &str = "ui";
+const UI_WATCH_DIR: &str = "ui/src";
+
+fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+
+    // Re-run build script if the UI code changes.
+    for entry in WalkDir::new(UI_WATCH_DIR) {
+        let entry = entry.expect("Could not read directory for adding to rerun-if-changed");
+        println!("cargo:rerun-if-changed={}", entry.path().display());
+    }
+
+    // Build the UI.
+    let output = Command::new("npm")
+        .current_dir(UI_DIR)
+        .arg("run")
+        .arg("build")
+        .output()
+        .expect("Failed to build UI");
+
+    assert!(
+        output.status.success(),
+        "Failed to build UI: npm exited with {:#?}",
+        output.status.code()
+    );
+}
