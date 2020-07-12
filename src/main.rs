9102297f102fce4603a5a74065fe810896b3ff34
Andrew Abbott <mirrexagon@mirrexagon.com>
2020-07-12 14:30:24 +1000

    Serialize board views

--- src/main.rs
+++ src/main.rs
@@ -18,19 +18,56 @@ fn get_board_view(
     filter: Option<String>,
     groupby: Option<String>,
 ) -> content::Json<String> {
-    // let view = if let Some(group_by_category) = groupby {
-    //     board.get_board_view_by_category(filter.as_deref(), &groupby.unwrap())
-    // } else {
-    //     board.get_board_view(filter.as_deref(), groupby.as_deref())
+    let json_output;
 
-    // };
+    if let Some(group_by_category) = groupby {
+        let view = board.get_board_view_by_category(filter.as_deref(), &group_by_category);
+        json_output = serde_json::to_string(&view).unwrap();
+    } else {
+        let view = board.get_board_view(filter.as_deref());
+        json_output = serde_json::to_string(&view).unwrap();
+    };
 
-    content::Json("".to_owned())
+    content::Json(json_output)
 }
 
 fn main() {
-    // rocket::ignite()
-    //     .mount("/", routes![get_board_view])
-    //     .manage(board)
-    //     .launch();
+    let mut board = Board::new(BoardId(0));
+
+    let card1 = board.add_card();
+    board.set_card_text(card1, "Task 1");
+    board.set_card_tags(
+        card1,
+        &[
+            Tag::from_tag_string("status:todo").unwrap(),
+            Tag::from_tag_string("artist:tester").unwrap(),
+            Tag::from_tag_string("rating:bad").unwrap(),
+        ],
+    );
+
+    let card2 = board.add_card();
+    board.set_card_text(card2, "Task 2");
+    board.set_card_tags(
+        card2,
+        &[
+            Tag::from_tag_string("status:in-progress").unwrap(),
+            Tag::from_tag_string("artist:tester").unwrap(),
+            Tag::from_tag_string("rating:bad").unwrap(),
+        ],
+    );
+
+    let card3 = board.add_card();
+    board.set_card_text(card3, "Task 3");
+    board.set_card_tags(
+        card3,
+        &[
+            Tag::from_tag_string("status:in-progress").unwrap(),
+            Tag::from_tag_string("rating:good").unwrap(),
+        ],
+    );
+
+    rocket::ignite()
+        .mount("/", routes![get_board_view])
+        .manage(board)
+        .launch();
 }
