32526d8afd6bd0d1c6cc48ac83b14b6a45809bc2
Andrew Abbott <mirrexagon@mirrexagon.com>
2020-07-12 21:28:11 +1000

    Finish skeleton app

---
+++ ui/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src",
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+        ],
+    }
+};
