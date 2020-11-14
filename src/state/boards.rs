b2768e7251cb030a919db5ff02515d573a9ed3cf
Andrew Abbott <mirrexagon@mirrexagon.com>
2020-11-14 20:22:09 +1100

    Update boards API

--- src/state/boards.rs
+++ src/state/boards.rs
@@ -1,14 +1,19 @@
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
 
 use super::board::{Board, BoardId};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Boards {
+    /// The list of boards.
     boards: Vec<Board>,
+
+    /// The ID that will be assigned to the next new board.
     next_board_id: BoardId,
 }
 
 impl Boards {
+    /// Create a new `Boards`.
     pub fn new() -> Self {
         Self {
             boards: Vec::new(),
@@ -16,46 +21,12 @@ impl Boards {
         }
     }
 
-    pub fn add_board(&mut self) -> &mut Board {
+    // Create a new board. Returns the ID of the new board, which can be used
+    // to retrieve it.
+    pub fn new_board(&mut self) -> BoardId {
         let id = self.get_next_board_id();
         self.boards.push(Board::new(id));
-
-        let index = self.boards.len() - 1;
-        &mut self.boards[index]
-    }
-
-    /// Returns `false` if no board with the given ID exists.
-    pub fn delete_board(&mut self, id: BoardId) -> bool {
-        let mut index = None;
-
-        for (i, board) in self.boards.iter().enumerate() {
-            if board.id() == id {
-                index = Some(i);
-                break;
-            }
-        }
-
-        if let Some(index) = index {
-            self.boards.remove(index);
-            true
-        } else {
-            false
-        }
-    }
-
-    pub fn get_boards(&self) -> &[Board] {
-        &self.boards[..]
-    }
-
-    /// Returns `None` if the specified board does not exist.
-    pub fn get_board_mut(&mut self, id: BoardId) -> Option<&mut Board> {
-        for board in &mut self.boards {
-            if board.id() == id {
-                return Some(board);
-            }
-        }
-
-        None
+        id
     }
 
     fn get_next_board_id(&mut self) -> BoardId {
@@ -63,4 +34,52 @@ impl Boards {
         self.next_board_id = self.next_board_id.next();
         next_board_id
     }
+
+    /// Delete the board with the specified ID.
+    pub fn delete_board(&mut self, id: BoardId) -> Result<(), BoardsError> {
+        if let Some(index) = self.get_index_of_board(id) {
+            self.boards.remove(index);
+            Ok(())
+        } else {
+            Err(BoardsError::NoSuchBoard)
+        }
+    }
+
+    /// Returns a reference to the board with the specified ID, or `None` if
+    /// the specified board does not exist.
+    pub fn get_board(&self, id: BoardId) -> Option<&Board> {
+        if let Some(index) = self.get_index_of_board(id) {
+            Some(&self.boards[index])
+        } else {
+            None
+        }
+    }
+
+    /// Returns a mutable reference to the board with the specified ID, or
+    /// `None` if the specified board does not exist.
+    pub fn get_board_mut(&mut self, id: BoardId) -> Option<&mut Board> {
+        if let Some(index) = self.get_index_of_board(id) {
+            Some(&mut self.boards[index])
+        } else {
+            None
+        }
+    }
+
+    /// Given a `BoardId`, return the index of the corresponding board in the
+    /// `boards` vector, or `None` if there is no such board.
+    fn get_index_of_board(&self, id: BoardId) -> Option<usize> {
+        for (i, board) in self.boards.iter().enumerate() {
+            if board.id() == id {
+                return Some(i);
+            }
+        }
+
+        None
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum BoardsError {
+    #[error("no such board was found")]
+    NoSuchBoard,
 }
