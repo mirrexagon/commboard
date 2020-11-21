fcf632c7bcdde5bec0f4d7eb94fb0903a45aba96
Andrew Abbott <mirrexagon@mirrexagon.com>
2020-11-21 15:25:48 +1100

    WIP: Add card tag manipulation

--- ui/src/Card.js
+++ ui/src/Card.js
@@ -4,18 +4,26 @@ import './Card.css';
 
 class Card extends React.Component {
     render() {
-        let tags = this.props.tags.map((tag) => <li key={tag}>{tag}</li>);
+        let tags = this.props.tags.map(
+            (tagString) => (<li className="card-tag" key={tagString}>
+                {tagString}
+            </li>));
 
         return (<div className="card-container">
             <p className="card-text">{this.props.text}</p>
-            <ul className="tag-list">{tags}</ul>
+            <ul className="card-tag-list">{tags}</ul>
         </div>);
     }
 }
 
 Card.propTypes = {
-    text: PropTypes.string,
-    tags: PropTypes.array,
+    text: PropTypes.string.isRequired,
+    tags: PropTypes.array.isRequired,
+
+    onSetCardText: PropTypes.func.isRequired,
+    onAddCardTag: PropTypes.func.isRequired,
+    onDeleteCardTag: PropTypes.func.isRequired,
+    onUpdateCardTag: PropTypes.func.isRequired,
 };
 
 export default Card
