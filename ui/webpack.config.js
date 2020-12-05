4d8397b05fcdf6874bb74edd1033f3523a84986b
Andrew Abbott <mirrexagon@mirrexagon.com>
2020-12-05 11:43:14 +1100

    Use swc instead of Babel

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
             {
                 test: /\.(js|jsx)/,
                 exclude: /node_modules/,
-                use: ["babel-loader"],
+                use: ["swc-loader"],
             },
             {
                 test: /\.css$/i,
