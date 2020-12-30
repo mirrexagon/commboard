04709e54a79c632b8bcf79a1e98254ca9aafef45
Andrew Abbott <mirrexagon@mirrexagon.com>
2020-12-30 11:31:10 +1100

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,12 +1,13 @@
 let
   nixpkgs-mozilla = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
   pkgs = import <nixpkgs> { overlays = [ nixpkgs-mozilla ]; };
-in
 
+  rustChannel = (pkgs.rustChannelOf { date = "2020-11-27"; channel = "nightly"; });
+  # rustChannels.nightly.rust
+in
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    (rustChannelOf { date = "2020-11-27"; channel = "nightly"; }).rust
-    # rustChannels.nightly.rust
+    rustChannel.rust
     cargo-edit
     nodejs
   ];
