c836332d589130ea1d11d06de0e8a157bfe692bb
Andrew Abbott <mirrexagon@mirrexagon.com>
2021-04-30 19:42:07 +1000

    Update Rust nightly version

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ let
   nixpkgs-mozilla = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
   pkgs = import <nixpkgs> { overlays = [ nixpkgs-mozilla ]; };
 
-  rustChannel = (pkgs.rustChannelOf { date = "2020-11-27"; channel = "nightly"; });
+  rustChannel = (pkgs.rustChannelOf { date = "2021-04-30"; channel = "nightly"; });
   # rustChannels.nightly.rust
 in
 pkgs.mkShell {
@@ -16,18 +16,4 @@ pkgs.mkShell {
 
   RUST_LOG = "info";
   NODE_ENV = "development";
-
-  shellHook = ''
-    uisetup() {
-      pushd ui
-      npm install
-      popd
-    }
-
-    uiserver() {
-      pushd ui
-      npm run start
-      popd
-    }
-  '';
 }
