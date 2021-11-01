a58b587d3532d569c09923512d7ab554e0feb49c
Andrew Abbott <andrew@mirx.dev>
2021-11-01 11:33:15 +1100

    Try to get textarea height to fill only parent

--- ui/src/CardFull.js
+++ ui/src/CardFull.js
@@ -36,13 +36,13 @@ const CardFull = (props) => {
             <div className="card-full-content">
                 <h3>{props.card.id}</h3>
 
-                <p className={`card-full-text_static card-full-text_static--${isEditingText ? "hidden" : "active"}`}>
+                <p className={`card-full-text_static--${isEditingText ? "hidden" : "active"}`}>
                     {props.card.text}
                 </p>
 
                 <textarea
                     ref={inputElement}
-                    className={`card-full-text_textarea card-full-text_textarea--${isEditingText ? "active" : "hidden"}`}
+                    className={`card-full-text_textarea--${isEditingText ? "active" : "hidden"}`}
                     value={editedText}
                     onChange={(e) => setEditedText(e.target.value)}
                 />
