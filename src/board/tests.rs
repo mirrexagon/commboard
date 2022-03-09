18d5eb93b5087243d119893f0bbb949b4f8bb732
Andrew Abbott <andrew@mirx.dev>
2022-03-09 19:45:58 +1100

    Add second test

--- src/board/tests.rs
+++ src/board/tests.rs
@@ -4,11 +4,6 @@ use super::*;
 
 fn new_board() -> Board {
     let file_path = path::Path::new("/tmp/board-test.json");
-
-    if file_path.exists() {
-        fs::remove_file(file_path).unwrap();
-    }
-
     Board::new(file_path)
 }
 
@@ -20,6 +15,30 @@ fn add_card_to_empty_board() -> Result<(), BoardError> {
 
     assert_eq!(board.cards.len(), 1);
     assert_eq!(board.card_order, vec![CardId::new(0)]);
+
+    Ok(())
+}
+
+#[test]
+fn move_card_within_order() -> Result<(), BoardError> {
+    let mut board = new_board();
+
+    board.perform_action(&Action::NewCard)?;
+    board.perform_action(&Action::NewCard)?;
+    board.perform_action(&Action::NewCard)?;
+
+    assert_eq!(
+        board.card_order,
+        vec![CardId::new(0), CardId::new(1), CardId::new(2)]
+    );
+
+    // Move last card up.
+    board.perform_action(&Action::MoveCurrentCardVerticalOffset { offset: -1 })?;
+
+    assert_eq!(
+        board.card_order,
+        vec![CardId::new(0), CardId::new(2), CardId::new(1)]
+    );
 
     Ok(())
 }
