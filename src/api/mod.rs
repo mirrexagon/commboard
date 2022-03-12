70996e2feb1ab18f7497776c738fe44aa8e3abf6
Andrew Abbott <andrew@mirx.dev>
2022-03-12 12:17:38 +1100

    Add timing for API methods

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,4 +1,7 @@
+use std::time::Instant;
 use std::sync::Mutex;
+
+use log::info;
 
 use rocket::{
     get, post,
@@ -19,9 +22,15 @@ pub fn validate_tag(tag: String) -> &'static str {
 
 #[get("/state")]
 pub fn get_current_state(board: &State<Mutex<board::Board>>) -> content::Json<String> {
-    let board = board.lock().unwrap();
+    let start = Instant::now();
 
-    content::Json(board.get_state_as_json().to_string())
+    let board = board.lock().unwrap();
+    let result = content::Json(board.get_state_as_json().to_string());
+
+    let duration = start.elapsed();
+    info!("/state took {} ms", duration.as_millis());
+
+    result
 }
 
 #[post("/action", data = "<action>")]
@@ -29,9 +38,16 @@ pub fn perform_action(
     board: &State<Mutex<board::Board>>,
     action: Json<board::Action>,
 ) -> Result<(), status::BadRequest<String>> {
+    let start = Instant::now();
+
     let mut board = board.lock().unwrap();
 
-    board
+    let result = board
         .perform_action(&action.0)
-        .map_err(|e| status::BadRequest(Some(format!("{}", e))))
+        .map_err(|e| status::BadRequest(Some(format!("{}", e))));
+
+    let duration = start.elapsed();
+    info!("/action took {} ms ", duration.as_millis());
+
+    result
 }
