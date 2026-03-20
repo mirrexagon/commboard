f4ef7e9426457105909af8dd23039ff2a2cd59b8
Andrew Abbott <andrew@mirx.dev>
2026-03-20 20:58:03 +1100

    Remove existing implementation

--- flake.nix
+++ flake.nix
@@ -2,9 +2,13 @@
   description = "Commboard, a multi-dimensional Kanban board";
 
   inputs = {
-    nixpkgs = { url = "nixpkgs/nixos-unstable"; };
+    nixpkgs = {
+      url = "nixpkgs/nixos-unstable";
+    };
 
-    flake-utils = { url = "github:numtide/flake-utils"; };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
 
     npmlock2nix = {
       url = "github:nix-community/npmlock2nix";
@@ -12,74 +16,83 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, npmlock2nix }: {
-    overlays.default = final: prev:
-      let
-        commboardDrv =
-          { stdenv
-          , lib
-          , rustPlatform
-          , fetchFromGitHub
-          , npmlock2nix
-          , nodejs
-          }:
-          rustPlatform.buildRustPackage rec {
-            pname = "commboard";
-            version = "git";
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      npmlock2nix,
+    }:
+    {
+      overlays.default =
+        final: prev:
+        let
+          commboardDrv =
+            {
+              stdenv,
+              lib,
+              rustPlatform,
+              fetchFromGitHub,
+              npmlock2nix,
+              nodejs,
+            }:
+            rustPlatform.buildRustPackage rec {
+              pname = "commboard";
+              version = "git";
 
-            src = ./.;
+              src = ./.;
 
-            nativeBuildInputs = [
-              nodejs
-            ];
+              nativeBuildInputs = [
+                nodejs
+              ];
 
-            cargoHash = "sha256-r0EAj0xTl3cSgUwFhEXuTthqEzy0YbeRe/mG0LnJQak=";
+              cargoHash = "sha256-r0EAj0xTl3cSgUwFhEXuTthqEzy0YbeRe/mG0LnJQak=";
 
-            nodeDependencies = npmlock2nix.v1.node_modules {
-              src = ./ui;
+              nodeDependencies = npmlock2nix.v1.node_modules {
+                src = ./ui;
+              };
+
+              preBuild = ''
+                ln -s $nodeDependencies/node_modules ./ui/node_modules
+                export PATH="$nodeDependencies/bin:$nodejs/bin:$PATH"
+              '';
+
+              doCheck = false;
+
+              meta = with lib; {
+                description = "A multi-dimensional Kanban board";
+                homepage = "https://github.com/mirrexagon/commboard";
+                license = with licenses; [ bsd0 ];
+                maintainers = with maintainers; [ mirrexagon ];
+              };
             };
 
-            preBuild = ''
-              ln -s $nodeDependencies/node_modules ./ui/node_modules
-              export PATH="$nodeDependencies/bin:$nodejs/bin:$PATH"
-            '';
-
-            doCheck = false;
-
-            meta = with lib; {
-              description = "A multi-dimensional Kanban board";
-              homepage = "https://github.com/mirrexagon/commboard";
-              license = with licenses; [ bsd0 ];
-              maintainers = with maintainers; [ mirrexagon ];
-            };
+        in
+        {
+          commboard = prev.callPackage commboardDrv {
+            npmlock2nix = (import npmlock2nix) { pkgs = final; };
           };
-
+        };
+    }
+    // flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        };
       in
       {
-        commboard = prev.callPackage commboardDrv {
-          npmlock2nix = (import npmlock2nix) { pkgs = final; };
+        packages.default = pkgs.commboard;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            deno
+          ];
+
+          NODE_ENV = "development";
         };
-      };
-  } // flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
-    in
-    {
-      packages.default = pkgs.commboard;
-
-      devShells.default =
-        pkgs.mkShell
-          {
-            buildInputs = with pkgs; [
-              rustc
-              cargo
-
-              cargo-edit
-
-              nodejs
-            ];
-
-            NODE_ENV = "development";
-          };
-    });
+      }
+    );
 }
--- src/config.rs
+++
@@ -1,7 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Deserialize)]
-pub struct Config {
-    #[serde(rename = "editor-command")]
-    editor_command: String,
-}
